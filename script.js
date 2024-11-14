6ae6bc0621e53600f8ec37f2c8fb1db9758b8b78
Patrick Brochu <patrickbrochu70@gmail.com>
2024-11-14 13:34:27 -0500

    Added the pen image, the buttons and text in each data cells using text input
    TODO: Find a way to make the input extend as the
    user type and overlap over the other cells with
    area element instead of text input

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let addRowButton = document.querySelector(".addButton")
-console.log(addRowButton)
-addRowButton.addEventListener("click", addRow)
-let tbody = document.querySelector("tbody")
+let addRowButton = document.querySelector(".addButton"),
+tbody = document.querySelector("tbody");
+addRowButton.addEventListener("click", addRow);
+
 
 
 
@@ -12,12 +12,32 @@ function addRow()
     let newRow = tbody.insertRow()
     for(let i = 0; i< 9; i++)
     {
-        let newCell = newRow.insertCell()
-        let editSymbol = document.createElement("button")
-        editSymbol.setAttribute("class", "editSymbol")
-        editSymbol.innerText = "Edit"
-        let newCellDivWrapper = document.createElement("div")
-        newCell.append(newCellDivWrapper)
-        newCellDivWrapper.append(editSymbol)
+        /*create row, cells and elements inside*/
+        let newCell = newRow.insertCell(),
+        editSymbol = document.createElement("button"),
+        newCellButtonWrapper = document.createElement("div"),
+        penImage = document.createElement("img"),
+        newCellTextWrapper = document.createElement("input");
+
+        /*Set up classes and attributes for inside elements*/
+        penImage.setAttribute("src", "img/pen.png");
+        editSymbol.setAttribute("class", "editSymbol");
+        newCellTextWrapper.setAttribute("type", "text");
+        newCellTextWrapper.setAttribute("class", "textCells");
+
+        /*Insert elements*/
+        editSymbol.append(penImage);
+        newCell.append(newCellButtonWrapper); 
+        newCellButtonWrapper.append(newCellTextWrapper);
+        newCellButtonWrapper.append(editSymbol);
+
+        /*add event listeners*/
+        editSymbol.addEventListener("click", editButtonClick)
+
     }
+}
+
+function editButtonClick(event)
+{
+
 }
